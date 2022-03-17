1dfeb8a15a0b7fb9cf1d68ebfd5e1ad8d245828b
Charles <charles.rousset@epfl.ch>
2022-03-17 12:00:20 +0100

    working on week4

--- Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
+++ Assets/Scripts/Core/Behaviors/MoveWithKeyboardBehavior.cs
@@ -15,6 +15,14 @@ public class MoveWithKeyboardBehavior : AgentBehaviour
     {
         Steering steering = new Steering();
         //implement your code here
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical= Input.GetAxis("Vertical");
+
+        print(vertical);
+
+       
+        steering.linear = new Vector3(horizontal, 0, vertical)* agent.maxAccel ;
+        steering.linear = this.transform.parent.TransformDirection(Vector3.ClampMagnitude(steering.linear , agent.maxAccel));         
         return steering;
     }
 
