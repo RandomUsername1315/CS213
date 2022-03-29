557c1660ccf93b103a3161bdaf7ba22dc4938030
Charles <charles.rousset@epfl.ch>
2022-03-29 23:49:26 +0200

    steering towards works

--- Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
+++ Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
@@ -27,19 +27,22 @@ public class GhostSheepBehavior : AgentBehaviour
         float distance = Mathf.Infinity;
         GameObject[] dogs = GameObject.FindGameObjectsWithTag("Dog");
         UnityEngine.Vector3 closest = new UnityEngine.Vector3(0, 0, 0);
+
         foreach (GameObject dog in dogs){
             float cur_distance = Vector3.Distance(position, dog.transform.position);
-            if (cur_distance < distance) {
+            if (cur_distance != 0 && cur_distance < distance) {
                 closest = dog.transform.position;
                 distance = cur_distance;
+                Debug.Log("New clossest");
             }
         }  
 
-        horizontal = 3 / (position.x - closest.x);
-        vertical = 3 / (position.y - closest.y); 
-        Console.WriteLine("({0}, {1})", horizontal, vertical);
+        
+        horizontal = 10 / (position.x - closest.x);
+        vertical = 10 / (position.z - closest.z); 
 
-        steering.linear = new Vector3(horizontal, 0, vertical)* agent.maxAccel;
+        steering.linear = new Vector3(horizontal, 0, vertical) * agent.maxAccel;
+
         steering.linear = this.transform.parent.TransformDirection (Vector3.ClampMagnitude(steering.linear , agent.maxAccel));
         return steering;
     }
